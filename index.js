806767ed3d3e572325703a4d876eaf8aaa905c3c
SpinnerMaster <spinnermaster@gmail.com>
2020-05-11 09:30:57 -0500

    cleaned up the code a bit

--- index.js
+++ index.js
@@ -1,60 +1,68 @@
 //-----the Discord function-----
-    function sendMessage() {
+function sendMessage() {
 //	  sendSlack();
-	  var sentence = document.getElementById("pingText").value;
-	  var color = document.getElementById("pPriority");
-	  var colorVal = color.options[color.selectedIndex].value;
-	  var alarmaText = " ";
-	  var alarma = document.getElementById("alarma").checked;
-	  if (alarma == true) {
+  var sentence = document.getElementById("pingText").value;
+  var color = document.getElementById("pPriority");
+  var colorVal = color.options[color.selectedIndex].value;
+  var alarmaText = " ";
+  var alarma = document.getElementById("alarma").checked;
+
+  if (alarma == true) {
 	  alarmaText = "<:frogsiren:708165935114223636> <:frogsiren:708165935114223636> <:frogsiren:708165935114223636> <:frogsiren:708165935114223636>"
 	  } else if (alarma == false) {
 	  alarmaText = ""
 	  };
-	  var hexSet = "";
-	  if (colorVal == "low") {
+
+  var hexSet = "";
+
+  if (colorVal == "low") {
 	  hexSet = "32768"
 	  } else if (colorVal == "high") {
 	  hexSet = "16753920"
 	  } else if (colorVal == "critical") {
 	  hexSet = "16711680"
 	  };
-    alertLevel = ""
-    if (colorVal == "low") {
+
+  if (colorVal == "low") {
     alertLevel = "here"
     } else if (colorVal == "high") {
     alertLevel = "channel"
     } else if (colorVal == "critical") {
     alertLevel = "everyone"
     };
-    var alertLevel = "";
-    if (colorVal == "low") {
+
+  var alertLevel = "";
+
+  if (colorVal == "low") {
     alertLevel = "here"
     } else if (colorVal == "high") {
     alertLevel = "channel"
     } else if (colorVal == "critical") {
     alertLevel = "everyone"
     };
-    var request = new XMLHttpRequest();
-	  var pingText = {
+
+  var request = new XMLHttpRequest();
+
+  var pingText = {
 	    title: "New ping!",
 		  description: (alertLevel + ' ' + sentence),
       color: (hexSet)
-    }
-    var alarmaHack = {
+    };
+  var alarmaHack = {
         title: "",
         description: (":thinking:" + " " + alarmaText),
         color: (hexSet)
-    }
+    };
 //INSERT URL FOR DISCORD IN THE QUOTES
-      request.open("POST", "https://discordapp.com/api/webhooks/708135837560209428/ReZIvRXFea-vpTJpvFpgogLXENAl0cgMeiLtNDMLGkwNeGQrBhUfphf761BJ2c5LMSz0");
-      request.setRequestHeader('Content-type', 'application/json');
-	  var params = {
+  request.open("POST", "");
+  request.setRequestHeader('Content-type', 'application/json');
+
+  var params = {
 		username: "Pingbot 2K20",
 		embeds: [ pingText, alarmaHack ]
 		}
       request.send(JSON.stringify(params));
-    }
+    };
 
 //-----the Slack function-----
 	function sendSlack() {
