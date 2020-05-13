b90e27b3d91ef3693f58a84aa2d99ace2b2cdeaa
ZhangC1459 <ZhangC1459@gmail.com>
2020-05-13 01:19:05 -0700

    Angular init
    Starting angular conversion

--- Legacy App/app.js
+++ Legacy App/app.js
@@ -1,13 +1,15 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+(function (__dirname){
 var fs = require("fs");
 var src = fs.readFileSync(__dirname + '/config.txt');
 
-fs.readFile('config.txt', function(err, data) {
-    if(err) throw err;
-    var webhookURL = data.toString().split("\n");
-    for(i in webhookURL) {
-        console.log(webhookURL[0]);
-    }
-});
+// fs.readFile('config.txt', function(err, data) {
+//     if(err) throw err;
+//     var webhookURL = data.toString().split("\n");
+//     for(i in webhookURL) {
+//         console.log(webhookURL[0]);
+//     }
+// });
 
 //-----the Discord function-----
 function sendMessage() {
@@ -65,7 +67,7 @@ function sendMessage() {
     };
 
 //INSERT URL FOR DISCORD IN THE QUOTES
-  request.open("POST", webhookURL[0]);
+  request.open("POST", "");
 
   request.setRequestHeader('Content-type', 'application/json');
 
@@ -127,3 +129,8 @@ function sendSlack() {
   console.log(params)
   request.send(JSON.stringify(params));
 };
+
+}).call(this,"/")
+},{"fs":2}],2:[function(require,module,exports){
+
+},{}]},{},[1]);
